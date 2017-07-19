ea1d8515a89472e1e253205de43280472f78e656
wuwujun <sqq0103@gmail.com>
2017-07-19 16:24:39 +0800

    branch1 first commit

--- app/src/main/java/com/example/shuqq/omds/activity_index.java
+++ app/src/main/java/com/example/shuqq/omds/activity_index.java
@@ -9,77 +9,49 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class activity_index extends AppCompatActivity {
-    Toolbar toolbar;
     WebView mWebview;
     WebSettings mWebSettings;
     final int version = Build.VERSION.SDK_INT;
 
-    String url="file:///android_asset/html/login.html";
+    String url="file:///android_asset/html/feedback.html";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
 
         mWebview = (WebView) findViewById(R.id.webview_index);
         mWebSettings = mWebview.getSettings();
         mWebSettings.setJavaScriptCanOpenWindowsAutomatically(true);
         mWebSettings.setLoadsImagesAutomatically(true);
         mWebSettings.setDefaultTextEncodingName("utf-8");
+        mWebSettings.setJavaScriptEnabled(true);
 
         mWebview.loadUrl(url);
-
-        String u = getIntent().getStringExtra("url");
-        if (!TextUtils.isEmpty(u)) {
-            mWebview.loadUrl(u);
-        }
 
         mWebview.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                if (url.contains("/signup")) {
-                    Intent intent = new Intent(activity_index.this, activity_index.class);
-                    intent.putExtra("url", "file:///android_asset/html/signup.html");
-                    startActivity(intent);
-                } else {
-                    Intent intent = new Intent(activity_index.this, activity_main.class);
-                }
-                view.loadUrl(url);
+                mWebview.loadUrl(url);
                 return true;
-            }
-        });
-
-        mWebview.setWebViewClient(new WebViewClient(){
-            @Override
-            public void  onPageStarted(WebView view, String url, Bitmap favicon) {
-                //设定加载开始的操作
-
-            }
-        });
-        mWebview.setWebChromeClient(new WebChromeClient() {
-            //获取网站标题
-            @Override
-            public void onReceivedTitle(WebView view, String title) {
-                toolbar.setTitle(title);
             }
         });
     }
 
 
-
-    //销毁Webview
     @Override
     protected void onDestroy() {
         if (mWebview != null) {
