ae579e5a315f262ea5dddb0cbf1d08e3c96164b1
wuwujun <sqq0103@gmail.com>
2017-07-23 20:13:50 +0800

    finish

--- app/src/main/java/com/example/shuqq/omds/activity_index.java
+++ app/src/main/java/com/example/shuqq/omds/activity_index.java
@@ -2,6 +2,7 @@ package com.example.shuqq.omds;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.http.SslError;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -10,9 +11,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.JavascriptInterface;
+import android.webkit.SslErrorHandler;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
@@ -25,7 +28,7 @@ public class activity_index extends AppCompatActivity {
     WebSettings mWebSettings;
     final int version = Build.VERSION.SDK_INT;
 
-    String url="file:///android_asset/html/feedback.html";
+    String url="https://123.206.92.43:8088/login/";
 
 
     @Override
@@ -34,21 +37,39 @@ public class activity_index extends AppCompatActivity {
         setContentView(R.layout.activity_index);
 
         mWebview = (WebView) findViewById(R.id.webview_index);
+
         mWebSettings = mWebview.getSettings();
         mWebSettings.setJavaScriptCanOpenWindowsAutomatically(true);
         mWebSettings.setLoadsImagesAutomatically(true);
         mWebSettings.setDefaultTextEncodingName("utf-8");
         mWebSettings.setJavaScriptEnabled(true);
 
-        mWebview.loadUrl(url);
 
         mWebview.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error){
+                handler.proceed();
+                Log.e("error","sslError:"+error.toString());
+            }
+
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 mWebview.loadUrl(url);
                 return true;
             }
+
         });
+
+        mWebview.loadUrl(url);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && mWebview.canGoBack()) {
+            mWebview.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 
 
